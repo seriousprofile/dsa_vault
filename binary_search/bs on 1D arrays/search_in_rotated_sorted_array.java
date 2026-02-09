ff5ba2cc83af299f6ee8c7a7e5a74b466149af6a
nando <166365496+seriousprofile@users.noreply.github.com>
2026-02-09 23:08:13 +0530

    Update search_in_rotated_sorted_array.java

--- binary_search/bs on 1D arrays/search_in_rotated_sorted_array.java
+++ binary_search/bs on 1D arrays/search_in_rotated_sorted_array.java
@@ -1,34 +1,41 @@
-//HINT: rotated arrays have a different understanding of low, mid and high pointers.
+// HINT: think about the index upto which the array is sorted.
 
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length; 
-        //int index = 0; 
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
 
-        int low = 0; 
-        int high = n - 1; 
-
-        while(low <= high){
-            int mid = low + (high - low) / 2; 
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
             if(nums[mid] == target){
                 return mid; 
             }
 
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target < nums[mid]){
-                    high = mid - 1; 
+            // if the first half is sorted
+            if (nums[low] <= nums[mid]) {
+
+                // and if the number is located in the sorted first half
+                if (nums[low] <= target && target <= nums[mid]) {
+                    // ignore the right half
+                    high = mid - 1;
                 } else {
-                    low = mid + 1; 
+                    low = mid + 1;
                 }
-            } else {
-                if(nums[mid] < target  && target <= nums[high]){
-                    low = mid + 1; 
+
+            } else { // if the second half is sorted
+
+                // and if the number is in the second sorted half
+                if (nums[mid] < target && target <= nums[high]) {
+                    // ignore the left half
+                    low = mid + 1;
                 } else {
-                    high = mid - 1; 
+                    high = mid - 1;
                 }
             }
-        } 
-        return -1; 
+        }
+
+        return -1;
     }
 }
