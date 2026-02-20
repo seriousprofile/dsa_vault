d080dfb02de10e3da098f8d603fd5bfa187fb94d
nando <166365496+seriousprofile@users.noreply.github.com>
2026-02-20 22:58:45 +0530

    Update find_the_difference.java

--- strings/basic and easy string problems /find_the_difference.java
+++ strings/basic and easy string problems /find_the_difference.java
@@ -3,6 +3,9 @@ class Solution {
         char[] sarr = s.toCharArray();
         char[] tarr = t.toCharArray();
 
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+
         for(int i = 0; i < sarr.length; i++){
             if(sarr[i] != tarr[i]){
                 return tarr[i];
