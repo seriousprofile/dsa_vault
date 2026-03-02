904b34607d80b658285e0feb3a0de53c4e07281e
nando <166365496+seriousprofile@users.noreply.github.com>
2026-03-02 20:12:58 +0530

    Update remove_duplicate_letters.java

--- strings/hard/remove_duplicate_letters.java
+++ strings/hard/remove_duplicate_letters.java
@@ -1,24 +1,32 @@
+// HINT : stack pattern + frequency 
+
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] freq = new int[26];
-        boolean[] inStack = new boolean[26];
+        int[] freq = new int[26]; //character appearance count 
+        boolean[] inStack = new boolean[26]; //is it in result? 
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); //stack for this problem :)
 
         for(char c : s.toCharArray()){
             freq[c - 'a']++;
-        }
+        } //count the frequencies of all chars 
 
         for(char c : s.toCharArray()){
-            int idx = c - 'a';
+            int idx = c - 'a'; 
+            freq[idx]--; //reduce the frequency 
 
-            freq[idx]--;
-
+            //if inStack == true which means it's in sb, then 
+            //we dont need recurring chars, so skip.
             if(inStack[idx]) continue; 
 
+            //otherwise:
             while(sb.length() > 0){
+
+                //first compare the last letter in sb:
                 char last = sb.charAt(sb.length() - 1);
 
+                //if the char is greater than curr and it appears again 
+                // we can definitely remove it and add it later. 
                 if(last > c && freq[last - 'a'] > 0){
                     inStack[last - 'a'] = false; 
                     sb.deleteCharAt(sb.length() - 1);
@@ -27,10 +35,13 @@ class Solution {
                 }
             }
 
+            //after deletion, append the current char since it is smaller 
+            // and change the flag to true as it is present in sb. 
             sb.append(c);
             inStack[idx] = true; 
         }
 
+        //now return sb. 
         return sb.toString();
     }
 }
