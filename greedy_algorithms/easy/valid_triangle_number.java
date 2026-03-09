ff24983d5c1e168efa4c0092568fff0783a32252
nando <166365496+seriousprofile@users.noreply.github.com>
2026-03-09 16:34:22 +0530

    Add valid_triangle_number.java for triangle counting
    Implement a solution to count valid triangle numbers from an array of integers.

---
+++ greedy_algorithms/easy/valid_triangle_number.java
@@ -0,0 +1,24 @@
+//HINT: when nums[left] + nums[right] > k then every index between left and right - 1 is also a valid triangle.
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0; 
+
+        for(int k = nums.length - 1; k >= 2; k--){
+            int left = 0; 
+            int right = k - 1; 
+
+            while(left < right){
+                if(nums[left] + nums[right] > nums[k]){
+                    count += right - left; 
+                    right--;
+                } else {
+                    left++; 
+                }
+            }
+        } 
+        
+        return count; 
+    }
+}
