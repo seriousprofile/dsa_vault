0ba8c8edbe348f33529f9464584bace51502fd10
nando <166365496+seriousprofile@users.noreply.github.com>
2026-03-21 23:10:45 +0530

    Create longest_repeating_character_replacement.java

---
+++ sliding_windows/medium/longest_repeating_character_replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) { 
+        int maxLen = 0; 
+        int left = 0; 
+        int maxFreq = 0;
+        int[] freq = new int[26];
+
+        for(int right = 0; right < s.length(); right++){
+
+            freq[s.charAt(right) - 'A']++;
+
+            maxFreq = Math.max(
+                maxFreq,
+                freq[s.charAt(right) - 'A']
+            );
+
+            while(right - left + 1 - maxFreq > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen; 
+    }
+}
